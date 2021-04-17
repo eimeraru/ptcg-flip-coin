28216d66df84a2cd5f02f50f9f1d879ac871743f
evdwarf <eimer.aru1127@gmail.com>
2021-04-17 22:00:16 +0900

    Add public function document comments

--- Sources/PTCGFlipCoin/PTCGFlipCoin.swift
+++ Sources/PTCGFlipCoin/PTCGFlipCoin.swift
@@ -1,25 +1,87 @@
 /**
+ * PTCGFlipCoinSummary
+ * コインを投げた表・裏の集計結果
+ */
+public struct PTCGFlipCoinSummary: CustomStringConvertible {
+
+    /**
+     * 表の出た数の合計
+     */
+    var headsCount: Int
+
+    /**
+     * 裏の出た数の合計
+     */
+    var tailCount: Int
+
+    init(_ heads: Int = 0, _ tail: Int = 0) {
+        headsCount = heads
+        tailCount = tail
+    }
+
+    public var description: String {
+        "PTCGFlipCoin - (heads: \(headsCount), tail: \(tailCount))"
+    }
+}
+
+public extension Array where Element == PTCGFlipCoin {
+    /**
+     * コインを投げた表と裏の合計を表す構造体を返す
+     */
+    func reducedFlipSummary() -> PTCGFlipCoinSummary {
+        return self.reduce(into: PTCGFlipCoinSummary()) { ( summary: inout PTCGFlipCoinSummary, fliped: PTCGFlipCoin) in
+            switch fliped {
+            case .heads:
+                summary.headsCount += 1
+            case .tail:
+                summary.tailCount += 1
+            }
+        }
+    }
+}
+
+/**
  * コインを投げる状態を表す
- * heads (表) / tail (裏)を表す
+ * heads or tail
  */
 public enum PTCGFlipCoin {
+    /**
+     コインの表
+     */
     case heads
+    
+    /**
+     コインの裏
+     */
     case tail
 
     public static func defaultGenerator() -> PTCGFlipCoin {
         [.heads, .tail].shuffled()[0]
     }
 
+    /**
+     * コインを1回投げ、その結果を返す
+     */
     public static func flip(_ generator: () -> PTCGFlipCoin = PTCGFlipCoin.defaultGenerator) -> PTCGFlipCoin {
         generator()
     }
 
+    /**
+     * コインを指定された回数投げ、その結果をまとめて返す
+     * - Parameters:
+     *   - count: 連続してコインを投げる回数
+     */
     public static func repeatWith(_ generator: () -> PTCGFlipCoin = PTCGFlipCoin.defaultGenerator, _ count: Int) -> Array<PTCGFlipCoin> {
         (0 ..< count).reduce(Array<PTCGFlipCoin>()) { (result, _) in
             result + [generator()]
         }
     }
 
+    /**
+     * 指定したコインの向きが出るまで繰り返しコインを投げ、その結果をまとめて返す
+     * - Parameters:
+     *   - untilFlip: 連続で投げ続ける条件となる指定したコインの向き
+     */
     public static func repeatUntil(_ generator: () -> PTCGFlipCoin = PTCGFlipCoin.defaultGenerator, _ untilFlip: PTCGFlipCoin) -> Array<PTCGFlipCoin> {
         var f = PTCGFlipCoin.flip(generator)
         var result = [f]
@@ -30,3 +92,4 @@ public enum PTCGFlipCoin {
         return result
     }
 }
+
--- Tests/PTCGFlipCoinTests/PTCGFlipCoinTests.swift
+++ Tests/PTCGFlipCoinTests/PTCGFlipCoinTests.swift
@@ -29,6 +29,21 @@ final class PTCGFlipCoinTests: XCTestCase {
             return current != limit ? .heads : .tail
         }, .tail), [.heads, .heads, .heads, .heads, .tail])
     }
+    
+    func testReducedSummary() {
+        let flipCoinResultsOfAllHeads: Array<PTCGFlipCoin> = [.heads, .heads, .heads]
+        var summary = flipCoinResultsOfAllHeads.reducedFlipSummary()
+        XCTAssertEqual(3, summary.headsCount)
+        XCTAssertEqual(0, summary.tailCount)
+        let flipCoinResultsOfAllTail: Array<PTCGFlipCoin> = [.tail, .tail, .tail, .tail]
+        summary = flipCoinResultsOfAllTail.reducedFlipSummary()
+        XCTAssertEqual(0, summary.headsCount)
+        XCTAssertEqual(4, summary.tailCount)
+        let flipCoinResultsOfMix: Array<PTCGFlipCoin> = [.tail, .heads, .tail, .tail]
+        summary = flipCoinResultsOfMix.reducedFlipSummary()
+        XCTAssertEqual(1, summary.headsCount)
+        XCTAssertEqual(3, summary.tailCount)
+    }
 
     static var allTests = [
         ("testFlip", testFlip),
